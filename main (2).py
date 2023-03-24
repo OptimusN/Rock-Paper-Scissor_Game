54d9f86b83c56c29b5e2695da7a6aa5359c025f3
Namus <110760279+OptimusN@users.noreply.github.com>
2023-03-24 00:47:50 -0700

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,53 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+
+game_images =[rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissor"))
+if user_choice >=3 or user_choice < 0:
+  print("You typed an invalid number, you lose!")
+else:
+  print(game_images[user_choice])
+  
+  computer_choice= random.randint(0, 2)
+  print(f"Computer chose: \n")
+  print(game_images[computer_choice])
+  
+  if user_choice == 0 and computer_choice ==2:
+    print("You Win!")
+  if computer_choice == 0 and user_choice ==2:
+    print("You Lose!")
+  if computer_choice > user_choice:
+    print("Computer Win")
+  if computer_choice < user_choice:
+    print("Computer Lose")
+  if user_choice == computer_choice:
+    print("It's a draw")
+  
